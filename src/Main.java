546f5c4a5aa45f826462c5c207c122d1b7248dad
Ejay Fernandez <ejaayfernandez@gmail.com>
2022-07-28 11:28:02 +0800

    YEY

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,7 @@ import java.text.DecimalFormat;
 
 
 import com.toedter.calendar.JDateChooser;
+import javax.swing.UIManager;
 
 public class Main{
 
@@ -59,17 +60,24 @@ public class Main{
 	    salary,
 	    utility;
 	float totalDebit = 0, totalCredit = 0;
+	float netIncome, totalExpenses;
+	float endingBalance;
+	float totalAssets, totalLiabs, totalFinal;
 
 
 	int ctr = 1;
 	
     JTable tableEntries = new JTable(new DefaultTableModel(new Object[]{"Entry No & Date","Account","Debit","Credit"},0));
     JTable tableTrial = new JTable(new DefaultTableModel(new Object[]{"Account","Debit","Credit"},0));
-    JTable tableStatement = new JTable(new DefaultTableModel(new Object[]{"",""},0));
+    JTable tableStatement = new JTable(new DefaultTableModel(new Object[]{" "," "},0));
+    JTable tableEquity = new JTable(new DefaultTableModel(new Object[]{" "," "},0));
+    JTable tablePosition = new JTable(new DefaultTableModel(new Object[]{" "," "},0));
     
 	DefaultTableModel entJournal = (DefaultTableModel) tableEntries.getModel();
 	DefaultTableModel entTrial = (DefaultTableModel) tableTrial.getModel();
 	DefaultTableModel entStatement = (DefaultTableModel) tableStatement.getModel();
+	DefaultTableModel entEquity = (DefaultTableModel) tableEquity.getModel();
+	DefaultTableModel entPosition = (DefaultTableModel) tablePosition.getModel();
 
     /**
 	 * Launch the application.
@@ -147,35 +155,35 @@ public class Main{
 		btnJournal.setForeground(Color.BLACK);
 		btnJournal.setFont(new Font("Montserrat", Font.BOLD, 17));
 		btnJournal.setBackground(new Color(255, 215, 0));
-		btnJournal.setBounds(-12, 49, 183, 81);
+		btnJournal.setBounds(-12, 68, 183, 63);
 		panelTabs.add(btnJournal);
 		
 		Button btnTrial = new Button("Trial Balance");
 		btnTrial.setForeground(Color.BLACK);
 		btnTrial.setFont(new Font("Montserrat", Font.BOLD, 17));
-		btnTrial.setBackground(SystemColor.inactiveCaptionBorder);
-		btnTrial.setBounds(-12, 130, 183, 81);
+		btnTrial.setBackground(UIManager.getColor("Button.disabledShadow"));
+		btnTrial.setBounds(-12, 150, 183, 55);
 		panelTabs.add(btnTrial);
 		
 		Button btnStatement = new Button("Income Statement");
 		btnStatement.setForeground(Color.BLACK);
 		btnStatement.setFont(new Font("Montserrat", Font.BOLD, 17));
-		btnStatement.setBackground(SystemColor.inactiveCaptionBorder);
-		btnStatement.setBounds(-12, 211, 183, 81);
+		btnStatement.setBackground(UIManager.getColor("Button.disabledShadow"));
+		btnStatement.setBounds(-12, 224, 183, 55);
 		panelTabs.add(btnStatement);
 		
 		Button btnEquity = new Button("Owner's Equity");
 		btnEquity.setForeground(Color.BLACK);
 		btnEquity.setFont(new Font("Montserrat", Font.BOLD, 17));
-		btnEquity.setBackground(SystemColor.inactiveCaptionBorder);
-		btnEquity.setBounds(-12, 292, 183, 81);
+		btnEquity.setBackground(UIManager.getColor("Button.disabledShadow"));
+		btnEquity.setBounds(-12, 298, 183, 55);
 		panelTabs.add(btnEquity);
 
 		Button btnPosition = new Button("Financial Position");
 		btnPosition.setForeground(Color.BLACK);
 		btnPosition.setFont(new Font("Montserrat", Font.BOLD, 17));
-		btnPosition.setBackground(SystemColor.inactiveCaptionBorder);
-		btnPosition.setBounds(-12, 373, 183, 81);
+		btnPosition.setBackground(UIManager.getColor("Button.disabledShadow"));
+		btnPosition.setBounds(-12, 372, 183, 55);
 		panelTabs.add(btnPosition);
 		
 		
@@ -221,7 +229,7 @@ public class Main{
 				}
 			});
 			btnClearJournal.setForeground(SystemColor.text);
-			btnClearJournal.setFont(new Font("Montserrat", Font.BOLD, 17));
+			btnClearJournal.setFont(new Font("Montserrat", Font.BOLD, 23));
 			btnClearJournal.setBackground(new Color(51,51,51));
 			btnClearJournal.setBounds(0, 412, 442, 49);
 			panelJournal.add(btnClearJournal);
@@ -317,6 +325,9 @@ public class Main{
 		panelStatement.setBounds(210, 100, 704, 461);
 		frame.getContentPane().add(panelStatement);
 		
+        JTableHeader tableHeader_Statement = tableStatement.getTableHeader();
+        tableHeader_Statement.setBackground(new Color(250,215,0)); // change the Background color
+        
 		Label lblStatement = new Label("INCOME STATEMENT");
 		lblStatement.setForeground(SystemColor.text);
 		lblStatement.setFont(new Font("Montserrat", Font.BOLD, 23));
@@ -326,30 +337,109 @@ public class Main{
 		panelStatement.add(lblStatement);
 
         JScrollPane tableScroll_Statement = new JScrollPane(tableStatement);        
-//        JTableHeader tableHeader_Statement = tableStatement.getTableHeader();
-
-//	        tableHeader_Statement.setBackground(new Color(250,215,0)); // change the Background color
-//	        tableHeader_Statement.setFont(new Font("Montserrat", Font.PLAIN, 17)); // font name style size
-//	        	((DefaultTableCellRenderer)tableHeader_Statement.getDefaultRenderer())
-//	                .setHorizontalAlignment(JLabel.CENTER); // center header text
-
 	        	centerRenderer.setHorizontalAlignment( JLabel.CENTER );
-	        
 	        	tableStatement.setFont(new Font("Montserrat", Font.PLAIN, 14));
-//	        tableHeader_Statement.setReorderingAllowed(false);
-//	        tableHeader_Statement.setResizingAllowed(false);
         	tableStatement.setRowHeight(30);
 
 	        entStatement.addRow(new Object[]{"Service Revenue", 0});
-	        entStatement.addRow(new Object[]{"EXPENSES  (LESS)", 0});
+	        entStatement.addRow(new Object[]{"EXPENSES  (LESS)----------------------------", 0});
 	        entStatement.addRow(new Object[]{"Rent Expense", 0});
 	        entStatement.addRow(new Object[]{"Salaries Expense", 0});
 	        entStatement.addRow(new Object[]{"Utilities Expense", 0, 0});
-	        entStatement.addRow(new Object[]{"NET INCOME  (LOSS)", 0, 0});
+	        entStatement.addRow(new Object[]{"NET INCOME  (LOSS)-------------------------", 0, 0});
 
 	    tableScroll_Statement.setBounds(21, 64, 660, 372);
         panelStatement.add(tableScroll_Statement);
         panelStatement.setVisible(false);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        JPanel panelEquity = new JPanel();
+		panelEquity.setLayout(null);
+		panelEquity.setBackground(Color.WHITE);
+		panelEquity.setBounds(210, 100, 704, 461);
+		frame.getContentPane().add(panelEquity);
+		
+        JTableHeader tableHeader_Equity = tableEquity.getTableHeader();
+        tableHeader_Equity.setBackground(new Color(250,215,0)); // change the Background color
+
+        Label lblEquity = new Label("CHANGES IN OWNER'S EQUITY");
+		lblEquity.setForeground(SystemColor.text);
+		lblEquity.setFont(new Font("Montserrat", Font.BOLD, 23));
+		lblEquity.setBackground(new Color(51, 51, 51));
+		lblEquity.setAlignment(Label.CENTER);
+		lblEquity.setBounds(0, 0, 704, 43);
+		panelEquity.add(lblEquity);
+
+        JScrollPane tableScroll_Equity = new JScrollPane(tableEquity);        
+	        	centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+	        	tableEquity.setFont(new Font("Montserrat", Font.PLAIN, 14));
+	        	tableEquity.setRowHeight(30);
+
+	        entEquity.addRow(new Object[]{"Beginning Capital Balance", 0});
+	        entEquity.addRow(new Object[]{"NET INCOME (ADD)--------------------------", 0});
+	        entEquity.addRow(new Object[]{"Investment", 0});
+	        entEquity.addRow(new Object[]{"Withdrawal", 0});
+	        entEquity.addRow(new Object[]{"CAPITAL BALANCE (END)--------------------", 0, 0});
+
+	        tableScroll_Equity.setBounds(21, 64, 660, 372);
+        panelEquity.add(tableScroll_Equity);
+        panelEquity.setVisible(false);
+        
+        
+        
+        
+        
+        
+        
+        
+        JPanel panelPosition = new JPanel();
+		panelPosition.setLayout(null);
+		panelPosition.setBackground(Color.WHITE);
+		panelPosition.setBounds(210, 100, 704, 461);
+		frame.getContentPane().add(panelPosition);
+		
+        JTableHeader tableHeader_Position = tablePosition.getTableHeader();
+        tableHeader_Position.setBackground(new Color(250,215,0)); // change the Background color
+
+        Label lblPosition = new Label("FINANCIAL POSITION");
+		lblPosition.setForeground(SystemColor.text);
+		lblPosition.setFont(new Font("Montserrat", Font.BOLD, 23));
+		lblPosition.setBackground(new Color(51, 51, 51));
+		lblPosition.setAlignment(Label.CENTER);
+		lblPosition.setBounds(0, 0, 704, 43);
+		panelPosition.add(lblPosition);
+
+        JScrollPane tableScroll_Position = new JScrollPane(tablePosition);        
+	        	centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+	        	tablePosition.setFont(new Font("Montserrat", Font.PLAIN, 14));
+	        	tablePosition.setRowHeight(30);
+
+	        //entPosition.addRow(new Object[]{"ASSETS------------------------------------", 0});
+	        entPosition.addRow(new Object[]{"Cash", 0});
+	        entPosition.addRow(new Object[]{"Accounts Receivable", 0});
+	        entPosition.addRow(new Object[]{"Supplies", 0});
+	        entPosition.addRow(new Object[]{"Land", 0});
+	        entPosition.addRow(new Object[]{"Equipment", 0});
+	        entPosition.addRow(new Object[]{"TOTAL ASSETS---------", 0});
+	        //entPosition.addRow(new Object[]{"LIABILITIES-------------------------------", 0});
+	        entPosition.addRow(new Object[]{"Unearned Income", 0});
+	        entPosition.addRow(new Object[]{"Accounts Payable", 0});
+	        entPosition.addRow(new Object[]{"TOTAL LIABILITIES-------", 0});
+	        entPosition.addRow(new Object[]{"Capital", 0});
+	        entPosition.addRow(new Object[]{"TOTAL LIABILITIES AND CAPITAL-------------", 0});
+
+	        tableScroll_Position.setBounds(170, 64, 350, 372);
+        panelPosition.add(tableScroll_Position);
+        panelPosition.setVisible(false);
+        
         
         
         
@@ -491,8 +581,8 @@ public class Main{
 					panelInputs.setVisible(true);
 				panelTrial.setVisible(false);
 				panelStatement.setVisible(false);
-				//panelPosition.setVisible(false);
-				//panelEquity.setVisible(false);
+				panelEquity.setVisible(false);
+				panelPosition.setVisible(false);
 			}
 		});
 		btnTrial.addActionListener(new ActionListener() {
@@ -507,8 +597,8 @@ public class Main{
 					panelInputs.setVisible(false);
 				panelTrial.setVisible(true);
 				panelStatement.setVisible(false);
-				//panelPosition.setVisible(false);
-				//panelEquity.setVisible(false);
+				panelEquity.setVisible(false);
+				panelPosition.setVisible(false);
 			}
 		});
 		btnStatement.addActionListener(new ActionListener() {
@@ -523,8 +613,8 @@ public class Main{
 					panelInputs.setVisible(false);
 				panelTrial.setVisible(false);
 				panelStatement.setVisible(true);
-				//panelPosition.setVisible(false);
-				//panelEquity.setVisible(false);
+				panelEquity.setVisible(false);
+				panelPosition.setVisible(false);
 			}
 		});
 		btnEquity.addActionListener(new ActionListener() {
@@ -535,12 +625,12 @@ public class Main{
 				btnEquity.setBackground(new Color(255, 215, 0));
 				btnPosition.setBackground(SystemColor.inactiveCaptionBorder);
 				
-				panelJournal.setVisible(true);
-					panelInputs.setVisible(true);
+				panelJournal.setVisible(false);
+					panelInputs.setVisible(false);
 				panelTrial.setVisible(false);
 				panelStatement.setVisible(false);
-				//panelPosition.setVisible(false);
-				//panelEquity.setVisible(false);
+				panelEquity.setVisible(true);
+				panelPosition.setVisible(false);
 			}
 		});
 		btnPosition.addActionListener(new ActionListener() {
@@ -551,12 +641,12 @@ public class Main{
 				btnEquity.setBackground(SystemColor.inactiveCaptionBorder);
 				btnPosition.setBackground(new Color(255, 215, 0));
 				
-				panelJournal.setVisible(true);
-					panelInputs.setVisible(true);
+				panelJournal.setVisible(false);
+					panelInputs.setVisible(false);
 				panelTrial.setVisible(false);
 				panelStatement.setVisible(false);
-				//panelPosition.setVisible(false);
-				//panelEquity.setVisible(false);
+				panelEquity.setVisible(false);
+				panelPosition.setVisible(true);
 			}
 		});
 
@@ -718,6 +808,47 @@ public class Main{
 			
 			tableTrial.getModel().setValueAt(fdf.format(totalDebit).toString(), 14, 1);
 			tableTrial.getModel().setValueAt(fdf.format(totalCredit).toString(), 14, 2);
+			
+			incomeStatement();
+	}
+	
+	public void incomeStatement() {
+		tableStatement.getModel().setValueAt(fdf.format(Math.abs(revenue)).toString(), 0, 1);
+		tableStatement.getModel().setValueAt(fdf.format(Math.abs(rent)).toString(), 2, 1);
+		tableStatement.getModel().setValueAt(fdf.format(Math.abs(salary)).toString(), 3, 1);
+		tableStatement.getModel().setValueAt(fdf.format(Math.abs(utility)).toString(), 4, 1);
+			totalExpenses = Math.abs(rent)+Math.abs(salary)+Math.abs(utility);
+			netIncome = Math.abs(revenue) - totalExpenses;
+		tableStatement.getModel().setValueAt(fdf.format(netIncome).toString(), 5, 1);
+		
+		ownerEquity();
+	}
+	
+	public void ownerEquity() {
+		tableEquity.getModel().setValueAt(fdf.format(Math.abs(netIncome)).toString(), 1, 1);
+		tableEquity.getModel().setValueAt(fdf.format(Math.abs(capital)).toString(), 2, 1);
+		tableEquity.getModel().setValueAt(fdf.format(Math.abs(drawing)).toString(), 3, 1);
+			endingBalance = netIncome + Math.abs(capital) - Math.abs(drawing);
+			tableEquity.getModel().setValueAt(fdf.format(Math.abs(drawing)).toString(), 4, 1);
+			
+		finPosition();
+	}
+	
+	public void finPosition() {
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(cash)).toString(), 0, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(accrec)).toString(), 1, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(supplies)).toString(), 2, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(land)).toString(), 3, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(equipment)).toString(), 4, 1);
+			totalAssets = Math.abs(cash) + Math.abs(accrec) + Math.abs(supplies) + Math.abs(land) + Math.abs(equipment);
+			tablePosition.getModel().setValueAt(fdf.format(Math.abs(totalAssets)).toString(), 5, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(unearned)).toString(), 6, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(accpay)).toString(), 7, 1);
+			totalLiabs = Math.abs(unearned) + Math.abs(accpay);
+			tablePosition.getModel().setValueAt(fdf.format(Math.abs(totalLiabs)).toString(), 8, 1);
+		tablePosition.getModel().setValueAt(fdf.format(Math.abs(capital)).toString(), 9, 1);
+			totalFinal = endingBalance + totalLiabs;
+			tablePosition.getModel().setValueAt(fdf.format(Math.abs(totalFinal)).toString(), 10, 1);
 	}
 	
 	public void reset() {
